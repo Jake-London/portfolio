61ddb784fbf246fffb229f2f50bf8480d26c39f2
Jake <jake.london@outlook.com>
2019-08-28 17:03:09 -0400

    Upload Files

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('Homepage requested');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'), (err) => {
+        if (err) throw err;
+    });
+});
+
+app.listen(5000, () => {
+    console.log('Listening on port 5000');
+})
---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+function showForm(formId) {
+    var element = document.getElementById('contact');
+
+    console.log('function fired');
+
+    if (element.style.display === 'flex') {
+        element.style.display = 'none';
+    }
+    else {
+        element.style.display = 'flex';
+    }
+}
