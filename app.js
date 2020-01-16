04b5a9eb435d75098e3d46a1637e0711e56df8f8
Jake London <jlondon@uoguelph.ca>
2020-01-16 15:33:32 -0500

    removed node_modules and added static file serving

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     console.log('Homepage requested');
     res.sendFile(path.join(__dirname, 'public', 'index.html'), (err) => {
