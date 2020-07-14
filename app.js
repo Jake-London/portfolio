4166fbb90af564b593492b83094eb40c0e4238c7
Jake London <jlondon@uoguelph.ca>
2020-07-13 21:05:30 -0400

    Removed Education, About, redesigned Projects section

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    console.log('Homepage requested');
-    res.sendFile(path.join(__dirname, 'public', 'index.html'), (err) => {
-        if (err) throw err;
-    });
+	console.log('Homepage requested');
+	res.sendFile(path.join(__dirname, 'public', 'index.html'), err => {
+		if (err) throw err;
+	});
 });
 
 app.listen(5000, () => {
-    console.log('Listening on port 5000');
-})
+	console.log('Listening on port 5000');
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,11 @@
 function showForm(formId) {
-    var element = document.getElementById('contact');
+	var element = document.getElementById('contact');
 
-    console.log('function fired');
+	console.log('function fired');
 
-    if (element.style.display === 'flex') {
-        element.style.display = 'none';
-    }
-    else {
-        element.style.display = 'flex';
-    }
+	if (element.style.display === 'flex') {
+		element.style.display = 'none';
+	} else {
+		element.style.display = 'flex';
+	}
 }
-
