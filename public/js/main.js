22c9892b100722f306641b50eb4ebd6f25ee5457
Jake London <jake.london@outlook.com>
2020-07-19 15:22:06 -0400

    updated showcase and landing section of home page

--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,50 @@ function showForm(formId) {
 		element.style.display = 'flex';
 	}
 }
+
+var elements = document.getElementsByClassName('project-img');
+console.log(elements);
+
+for (let e of elements) {
+	let animStatus = false;
+
+	e.addEventListener('mouseenter', () => {
+		console.log('Entered project-img div');
+		let imgSplash = e.getElementsByClassName('img-splash')[0];
+		imgSplash.classList.remove('img-splash-hide');
+		imgSplash.classList.add('img-splash-show');
+
+		let child = imgSplash.children[0];
+		console.log(child);
+
+		child.classList.add('reveal-splash-text');
+
+		/* let children = imgSplash.children;
+		console.log(children);
+		for (let child of children) {
+			child.style.opacity = '1';
+		} */
+	});
+
+	e.addEventListener('mouseleave', () => {
+		console.log('Leaving project-img div');
+		let imgSplash = e.getElementsByClassName('img-splash')[0];
+		imgSplash.classList.add('img-splash-hide');
+		imgSplash.classList.remove('img-splash-show');
+
+		let child = imgSplash.children[0];
+
+		child.classList.remove('reveal-splash-text');
+		/* child.style.display = 'none';
+		child.style.opacity = '0'; */
+		/* let children = imgSplash.children;
+		console.log(children);
+		for (let child of children) {
+			child.style.opacity = '0';
+		} */
+	});
+}
+
+var links = document.querySelectorAll('a.req-anim');
+
+console.log(links);
